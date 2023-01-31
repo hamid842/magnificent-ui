11561e6d947e70b2ad077c024f561692f7b1416e
hamid-mobilityone <hamid@mobilityonegroup.com>
2023-02-01 01:53:42 +0330

    [FIX] footer import fixed

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Header from '@/components/Header'
 import React from "react";
-import Footer from "@/components/Footer";
+import Footer from "./Footer";
 
 export default function Layout({children}: { children: React.ReactNode }) {
     return (
