d8479ef1e8d9b4ceb073ecd33e49dcb8af6bb545
Mehdi <mjv@medals.ai>
2023-02-14 18:10:20 +0330

    style

--- src/pages/rent-your-place/form.tsx
+++ src/pages/rent-your-place/form.tsx
@@ -1,10 +1,17 @@
 import StepsComponent from "@/components/rent-your-place/StepsComponent";
+import { ReactElement } from "react-markdown/lib/react-markdown";
+import Layout from "@/components/global/Layout";
+import AppContainer from "@/components/global/AppContainer";
+import { Typography } from "@mui/material";
 
 const RentPlaceForm = () => {
-    return (
-        <>
-        <StepsComponent />
-        </>
-    )
-}
+  return (
+    <>
+      <Typography variant={"h6"} sx={{ mt: 20 }}></Typography>
+      <StepsComponent />
+    </>
+  );
+};
 export default RentPlaceForm;
+
+RentPlaceForm.getLayout = (page: ReactElement) => <Layout> {page}</Layout>;
