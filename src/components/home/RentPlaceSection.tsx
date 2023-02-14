5750c329df1f93b6728b779db822088c471424ba
hamid-mobilityone <hamid@mobilityonegroup.com>
2023-02-14 18:54:49 +0330

    images changed

--- src/components/home/RentPlaceSection.tsx
+++ src/components/home/RentPlaceSection.tsx
@@ -4,7 +4,7 @@ import {useRouter} from "next/router";
 // Material-ui
 import {Box, Grid} from "@mui/material";
 // Project imports
-import rentImage from '../../../public/rent.png'
+import rentImage from '../../../public/8/8-2-1-768x568.jpg'
 import TitleSeparator from "@/components/global/TitleSeparator";
 import dictionaries from '../../../dictionaries/en.json';
 import AppButton from "@/components/global/AppButton";
@@ -19,7 +19,7 @@ const RentPlaceSection = () => {
             <TitleSeparator separatorTitle={'With us'} title={'Rent Your Place'}/>
             <Grid container spacing={3}>
                 <Grid item xs={12} sm={6} lg={6}>
-                    <Image src={rentImage} alt={"Pic"} width={400} height={400} style={{background: "transparent"}}/>
+                    <Image src={rentImage} alt={"Pic"} width={400} height={310} style={{background: "transparent"}}/>
                 </Grid>
                 <Grid item xs={12} sm={6} lg={6}>
                     <SwitzerText sx={{textAlign: 'justify'}}
