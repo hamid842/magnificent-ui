3c8a72b6a2432acef7d8c4731b810e1b95c1f409
Mehdi <mjv@medals.ai>
2023-02-15 11:42:27 +0330

    style

--- src/components/blog/BlogItem.tsx
+++ src/components/blog/BlogItem.tsx
@@ -1,69 +1,124 @@
 import Image from "next/image";
-import {Box, Button, Grid, Paper, Stack, Typography} from "@mui/material";
-import {IBlogPostsData} from "@/utils/blog-post-type";
+import { Box, Button, Grid, Paper, Stack, Typography } from "@mui/material";
+import { IBlogPostsData } from "@/utils/blog-post-type";
 import EuclidText from "@/components/css-texts/EuclidText";
-import blogOneImage from '../../../public/Blogs/11/02-S.jpg'
-import blogTwoImage from '../../../public/Blogs/11/3-b.jpg'
-import blogThreeImage from '../../../public/Blogs/11/4-b.jpg'
-import blogFourImage from '../../../public/Blogs/11/04-S.jpg'
-import blogImageFive from '../../../public/Blogs/11/1-b.jpg'
+import blogOneImage from "../../../public/Blogs/11/02-S.jpg";
+import blogTwoImage from "../../../public/Blogs/11/3-b.jpg";
+import blogThreeImage from "../../../public/Blogs/11/4-b.jpg";
+import blogFourImage from "../../../public/Blogs/11/04-S.jpg";
+import blogImageFive from "../../../public/Blogs/11/1-b.jpg";
 import colors from "@/assets/colors";
 
 type BlogItemProps = {
-    blogData: IBlogPostsData,
-}
+  blogData: IBlogPostsData;
+};
 
-const BlogItem = ({blogData}: BlogItemProps) => {
-    const {attributes} = blogData;
-    const IMAGE_WIDTH = 100;
-    const IMAGE_HEIGHT = 100;
-    const imageStyles = {width: '100%', height: 200, borderBottomLeftRadius: 6, borderTopLeftRadius: 6}
-    return (
-        <Paper elevation={3}
-               sx={{
-                   position: 'relative',
-                   width: '100%',
-                   borderRadius: 2,
-                   height: 200
-               }}>
-            <Grid container>
-                <Grid item xs={12} sm={12} md={6} lg={6}>
-                    <Box>
-                        {blogData.id === 1 &&
-                            <Image src={blogOneImage} alt={'Pic'} width={IMAGE_WIDTH} height={IMAGE_HEIGHT}
-                                   style={imageStyles}/>}
-                        {blogData.id === 2 &&
-                            <Image src={blogTwoImage} alt={'Pic'} width={IMAGE_WIDTH} height={IMAGE_HEIGHT}
-                                   style={imageStyles}/>}
-                        {blogData.id === 3 &&
-                            <Image src={blogThreeImage} alt={'Pic'} width={IMAGE_WIDTH} height={IMAGE_HEIGHT}
-                                   style={imageStyles}/>}
-                        {blogData.id === 4 &&
-                            <Image src={blogFourImage} alt={'Pic'} width={IMAGE_WIDTH} height={IMAGE_HEIGHT}
-                                   style={imageStyles}/>}
-                        {blogData.id === 5 &&
-                            <Image src={blogImageFive} alt={'Pic'} width={IMAGE_WIDTH} height={IMAGE_HEIGHT}
-                                   style={imageStyles}/>}
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={12} md={6} lg={6} p={1}>
-                    <Stack alignItems={'center'}>
-                        <EuclidText textAlign={'center'} text={attributes.title.toLowerCase()}
-                                    sx={{fontWeight: 700,my:0.5, fontSize: 14, textTransform: 'capitalize !important'}}/>
-                        <Box>
-                            <Typography variant={'caption'}
-                                        sx={{fontSize: 10, textAlign: 'justify'}}>{attributes.summary}</Typography>
-                        </Box>
-                    </Stack>
-                    <Button sx={{
-                        color: colors.mainColor,
-                        textTransform: 'none',
-                        fontSize: 12,
-                    }} >Read more</Button>
-                </Grid>
-            </Grid>
-
-        </Paper>
-    )
-}
+const BlogItem = ({ blogData }: BlogItemProps) => {
+  const { attributes } = blogData;
+  const IMAGE_WIDTH = 100;
+  const IMAGE_HEIGHT = 100;
+  const imageStyles = {
+    width: "100%",
+    height: 200,
+    borderBottomLeftRadius: 6,
+    borderTopLeftRadius: 6,
+  };
+  return (
+    <Paper
+      elevation={3}
+      sx={{
+        position: "relative",
+        width: "100%",
+        borderRadius: 2,
+        height: 200,
+      }}
+    >
+      <Grid container>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          <Box>
+            {blogData.id === 1 && (
+              <Image
+                src={blogOneImage}
+                alt={"Pic"}
+                width={IMAGE_WIDTH}
+                height={IMAGE_HEIGHT}
+                style={imageStyles}
+              />
+            )}
+            {blogData.id === 2 && (
+              <Image
+                src={blogTwoImage}
+                alt={"Pic"}
+                width={IMAGE_WIDTH}
+                height={IMAGE_HEIGHT}
+                style={imageStyles}
+              />
+            )}
+            {blogData.id === 3 && (
+              <Image
+                src={blogThreeImage}
+                alt={"Pic"}
+                width={IMAGE_WIDTH}
+                height={IMAGE_HEIGHT}
+                style={imageStyles}
+              />
+            )}
+            {blogData.id === 4 && (
+              <Image
+                src={blogFourImage}
+                alt={"Pic"}
+                width={IMAGE_WIDTH}
+                height={IMAGE_HEIGHT}
+                style={imageStyles}
+              />
+            )}
+            {blogData.id === 5 && (
+              <Image
+                src={blogImageFive}
+                alt={"Pic"}
+                width={IMAGE_WIDTH}
+                height={IMAGE_HEIGHT}
+                style={imageStyles}
+              />
+            )}
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6} p={1}>
+          <Stack alignItems={"center"}>
+            <EuclidText
+              textAlign={"center"}
+              text={attributes.title.toLowerCase()}
+              sx={{
+                fontWeight: 700,
+                my: 0.5,
+                fontSize: 14,
+                textTransform: "capitalize !important",
+              }}
+            />
+            <Box>
+              <Typography
+                variant={"caption"}
+                sx={{ fontSize: 10, textAlign: "justify" }}
+              >
+                {attributes.summary.substring(
+                  0,
+                  attributes.summary.length - 200
+                )}
+              </Typography>
+            </Box>
+          </Stack>
+          <Button
+            sx={{
+              color: colors.mainColor,
+              textTransform: "none",
+              fontSize: 12,
+            }}
+          >
+            Read more
+          </Button>
+        </Grid>
+      </Grid>
+    </Paper>
+  );
+};
 export default BlogItem;
--- src/pages/blogs.tsx
+++ src/pages/blogs.tsx
@@ -1,54 +1,65 @@
 // Next.js
-import {useRouter} from "next/router";
-import {GetServerSideProps} from "next";
+import { useRouter } from "next/router";
+import { GetServerSideProps } from "next";
 // Material ui
-import {Grid} from "@mui/material";
+import { Grid } from "@mui/material";
 // Project imports
-import {instance} from "@/config/axiosConfig";
-import {IBlogPostsData} from "@/utils/blog-post-type";
+import { instance } from "@/config/axiosConfig";
+import { IBlogPostsData } from "@/utils/blog-post-type";
 import BlogItem from "@/components/blog/BlogItem";
 import AppContainer from "@/components/global/AppContainer";
 import TitleSeparator from "@/components/global/TitleSeparator";
-import {ReactElement} from "react";
+import { ReactElement } from "react";
 import Layout from "@/components/global/Layout";
 import HomePage from "@/pages/index";
 
 export const getServerSideProps: GetServerSideProps = async () => {
-    const {data} = await instance.get(`/blog-posts`)
-    const blogPosts = data.data
-    return {
-        props: {
-            blogPosts
-        }
-    }
-}
+  const { data } = await instance.get(`/blog-posts`);
+  const blogPosts = data.data;
+  return {
+    props: {
+      blogPosts,
+    },
+  };
+};
 
 type BlogProps = {
-    blogPosts: IBlogPostsData[]
-}
+  blogPosts: IBlogPostsData[];
+};
 
 //========================|| Blogs ||=============================
 
-const Blogs = ({blogPosts}: BlogProps) => {
-    const router = useRouter();
-    return (
-        <AppContainer>
-            <TitleSeparator separatorTitle={''} title={'Blogs'} mt={8}/>
-            <Grid container spacing={2}>
-                {blogPosts?.map(post =>
-                    <Grid key={post.id} item xs={12} sm={6} lg={6}
-                          onClick={() => router.push({
-                              pathname: `/blogs/${post.id}`,
-                              query: {id: JSON.stringify(post.id)}
-                          }, `/blogs/${post.id}`)} sx={{cursor: 'pointer'}}
-                    >
-                        <BlogItem blogData={post} />
-                    </Grid>
-                )}
-            </Grid>
-        </AppContainer>
-    )
-}
+const Blogs = ({ blogPosts }: BlogProps) => {
+  const router = useRouter();
+  return (
+    <AppContainer>
+      <TitleSeparator separatorTitle={""} title={"Blogs"} mt={8} />
+      <Grid container spacing={2}>
+        {blogPosts?.map((post) => (
+          <Grid
+            key={post.id}
+            item
+            xs={12}
+            sm={6}
+            lg={6}
+            onClick={() =>
+              router.push(
+                {
+                  pathname: `/blogs/${post.id}`,
+                  query: { id: JSON.stringify(post.id) },
+                },
+                `/blogs/${post.id}`
+              )
+            }
+            sx={{ cursor: "pointer" }}
+          >
+            <BlogItem blogData={post} />
+          </Grid>
+        ))}
+      </Grid>
+    </AppContainer>
+  );
+};
 export default Blogs;
 
-Blogs.getLayout = (page:ReactElement)=><Layout>{page}</Layout>
+Blogs.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
