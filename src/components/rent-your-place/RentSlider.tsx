296c99dcec185c2ac665f5d2d883739db6eeef17
Mehdi <mjv@medals.ai>
2023-02-19 13:29:54 +0330

    about

--- src/components/rent-your-place/RentSlider.tsx
+++ src/components/rent-your-place/RentSlider.tsx
@@ -1,68 +1,76 @@
-import {Box, Slider} from "@mui/material";
-import {styled} from "@mui/material/styles";
-import {useState} from "react";
+import { Box, Slider } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useState } from "react";
 import SwitzerText from "@/components/css-texts/SwitzerText";
 
-
 const ReSlider = styled(Slider)({
-    color: 'black',
-    marginTop: 10,
-    marginBottom: 10,
-    height: 5,
-    '& .MuiSlider-track': {
-        border: 'none',
+  color: "black",
+  marginTop: 10,
+  marginBottom: 10,
+  height: 5,
+  "& .MuiSlider-track": {
+    border: "none",
+  },
+  "& .MuiSlider-rail": {
+    color: "black",
+  },
+  "& .MuiSlider-thumb": {
+    height: 30,
+    width: 30,
+    backgroundColor: "#FEBE1E",
+    "&:before": {
+      display: "none",
     },
-    '& .MuiSlider-rail': {
-        color: 'black'
+  },
+  "& .MuiSlider-valueLabel": {
+    lineHeight: 1.2,
+    fontSize: 12,
+    background: "unset",
+    padding: 0,
+    width: 32,
+    height: 32,
+    borderRadius: "50% 50% 50% 0",
+    backgroundColor: "#52af77",
+    transformOrigin: "bottom left",
+    transform: "translate(50%, -100%) rotate(-45deg) scale(0)",
+    "&:before": { display: "none" },
+    "&.MuiSlider-valueLabelOpen": {
+      transform: "translate(50%, -100%) rotate(-45deg) scale(1)",
     },
-    '& .MuiSlider-thumb': {
-        height: 30,
-        width: 30,
-        backgroundColor: '#FEBE1E',
-        '&:before': {
-            display: 'none',
-        },
+    "& > *": {
+      transform: "rotate(45deg)",
     },
-    '& .MuiSlider-valueLabel': {
-        lineHeight: 1.2,
-        fontSize: 12,
-        background: 'unset',
-        padding: 0,
-        width: 32,
-        height: 32,
-        borderRadius: '50% 50% 50% 0',
-        backgroundColor: '#52af77',
-        transformOrigin: 'bottom left',
-        transform: 'translate(50%, -100%) rotate(-45deg) scale(0)',
-        '&:before': {display: 'none'},
-        '&.MuiSlider-valueLabelOpen': {
-            transform: 'translate(50%, -100%) rotate(-45deg) scale(1)',
-        },
-        '& > *': {
-            transform: 'rotate(45deg)',
-        },
-    },
+  },
 });
 
 const RentSlider = () => {
-    const [sliderValue, setSliderValue] = useState(0)
+  const [sliderValue, setSliderValue] = useState(0);
 
-    const handleChangeSlider = (event: Event, newValue: number | number[]) => {
-        setSliderValue(newValue as number);
-    };
+  const handleChangeSlider = (event: Event, newValue: number | number[]) => {
+    setSliderValue(newValue as number);
+  };
 
-    return (
-        <>
-            <Box>
-                <SwitzerText sx={{mt: 2}} text={'You could earn'}/>
-                <SwitzerText align={'center'} text={'1,200 AED'}/>
-            </Box>
-            <Box>
-                <SwitzerText sx={{mt: 2}} text={`${sliderValue} nights at an estimated ${sliderValue * 1200} AED a
-                    night`}/>
-            </Box>
-            <ReSlider min={0} max={10} value={sliderValue} onChange={handleChangeSlider}/>
-        </>
-    )
-}
+  return (
+    <>
+      <Box>
+        <SwitzerText sx={{ mt: 2 }} text={"You could earn"} />
+        <SwitzerText align={"center"} text={"12,000 AED"} />
+      </Box>
+      <Box>
+        <SwitzerText
+          sx={{ mt: 2 }}
+          text={`${sliderValue} nights at an estimated ${
+            sliderValue * 1200
+          } AED per night`}
+        />
+      </Box>
+      <ReSlider
+        min={0}
+        max={10}
+        value={sliderValue}
+        onChange={handleChangeSlider}
+      />
+    </>
+  );
+};
 export default RentSlider;
--- src/pages/rent-your-place.tsx
+++ src/pages/rent-your-place.tsx
@@ -1,127 +1,194 @@
-import {ReactElement} from "react";
+import { ReactElement } from "react";
 // Material ui
-import {Box, Grid, Stack, Typography} from "@mui/material";
+import { Box, Grid, Stack, Typography } from "@mui/material";
 // Next.js
 import Image from "next/image";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 // Project imports
 import OurPartnership from "@/components/global/OurPartnership";
 import RentSlider from "@/components/rent-your-place/RentSlider";
-import rentMiddleImage from '../../public/rent-page-middle.png'
-import travelPic from '../../public/ship.png';
-import cityPic from '../../public/city.png';
-import islandPic from '../../public/beach.png';
-import explorePic from '../../public/explore.png';
-import {ColorButton} from "@/components/global/Header";
+import rentMiddleImage from "../../public/rent-page-middle.png";
+import travelPic from "../../public/ship.png";
+import cityPic from "../../public/city.png";
+import islandPic from "../../public/beach.png";
+import explorePic from "../../public/explore.png";
+import { ColorButton } from "@/components/global/Header";
 import Layout from "@/components/global/Layout";
 import AppContainer from "@/components/global/AppContainer";
 import EuclidText from "@/components/css-texts/EuclidText";
 import colors from "@/assets/colors";
 
 const RentYourPlace = () => {
-    const router = useRouter()
+  const router = useRouter();
 
-    return (
-        <>
-            <AppContainer>
-                <Grid container spacing={3} mt={15}>
-                    <Grid item xs={12} sm={6} lg={5}>
-                        <Stack direction={'column'} alignItems={'center'} justifyContent={'space-between'}>
-                            <EuclidText variant={'h4'} sx={{fontWeight: 600, mt: 12}} text={'Rent it !'}/>
-                            <RentSlider/>
-                        </Stack>
-                        <Box mt={5}>
-                            <OurPartnership/>
-                        </Box>
-                    </Grid>
-                    <Grid item xs={12} sm={6} lg={7}>
-                        <Box sx={{
-                            p: 2,
-                            width: '100%',
-                            height: '100%',
-                            backgroundColor: colors.mainColor,
-                            borderTopLeftRadius: 50,
-                            boxShadow: 'rgb(0 0 0 / 25%) 0 14px 45px, rgb(0 0 0 / 22%) 0 10px 18px',
-                        }}>
-                            <Box sx={{
-                                width: '100%',
-                                height: '100%',
-                                borderTopLeftRadius: 50,
-                            }}>
-                                <Image
-                                    src={'https://cdn.leonardo.ai/users/2dc5dbea-cfc5-4a04-a311-747595418e49/generations/fc058562-9c90-488e-9c44-6d003fc800ba/Leonardo_Creative_Home_rental_about_us_page_photo_0.jpg'}
-                                    width={200} height={200} alt={'Rent'} style={{
-                                    background: "transparent",
-                                    borderTopLeftRadius: 50,
-                                    borderBottomRightRadius: 50,
-                                    width: '100%',
-                                    height: '100%'
-                                }}/>
-                            </Box>
-                        </Box>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={2} my={5}>
-                    <Grid item xs={12} sm={6} md={6} lg={6}>
-                        <Image src={rentMiddleImage} width={400} height={400} alt={'Rent'}/>
-                    </Grid>
-                    <Grid item xs={12} sm={6} md={6} lg={6}>
-                        <Typography variant={'caption'} sx={{fontWeight: 600}}>Rent your place</Typography>
-                        <Typography variant={'h6'} sx={{fontWeight: 700, marginY: 2}}>One-to-one guidance from a
-                            superhost</Typography>
-                        <Typography variant={'caption'}>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Architecto, assumenda et
-                            facilis in inventore ipsa maxime neque quibusdam rem saepe, suscipit ullam veritatis! Cum
-                            fugit
-                            nam nostrum sint! Distinctio, minus.</Typography>
-                        <Grid container my={2}>
-                            <Grid item xs={6}>
-                                <Typography sx={{fontWeight: 600}}>100+</Typography>
-                                <Typography variant={'caption'} sx={{fontSize: 10, lineHeight: 2}}>Lorem</Typography>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <Typography sx={{fontWeight: 600}}>172</Typography>
-                                <Typography variant={'caption'} sx={{fontSize: 10, lineHeight: 2}}>Places</Typography>
-                            </Grid>
-                            <Grid item xs={6} py={1}>
-                                <Typography sx={{fontWeight: 600}}>68</Typography>
-                                <Typography variant={'caption'} sx={{fontSize: 10, lineHeight: 2}}>Lorem</Typography>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <Typography sx={{fontWeight: 600}}>32M+</Typography>
-                                <Typography variant={'caption'} sx={{fontSize: 10, lineHeight: 2}}>We help to find your
-                                    dream place</Typography>
-                            </Grid>
-                        </Grid>
-                        <ColorButton size={'small'} variant={'contained'}
-                                     sx={{borderRadius: 25, m: 1, textTransform: "capitalize"}}
-                                     onClick={() => router.push('/rent-your-place/form')}>Rent
-                            your place</ColorButton>
-                    </Grid>
-                </Grid>
-                <Stack direction={'row'} justifyContent={'space-between'} pb={5}>
-                    <Stack direction={'column'} alignItems={'center'} sx={{color: '#A8A7AB'}}>
-                        <Image src={travelPic} alt={'Travel'} width={100} height={100}/>
-                        <Typography mt={1}>TRAVEL</Typography>
-                    </Stack>
-                    <Stack direction={'column'} alignItems={'center'} sx={{color: 'gray'}}>
-                        <Image src={cityPic} alt={'Travel'} width={100} height={100}/>
-                        <Typography mt={1}>CITY TOUR</Typography>
-                    </Stack>
-                    <Stack direction={'column'} alignItems={'center'} sx={{color: 'gray'}}>
-                        <Image src={islandPic} alt={'Travel'} width={100} height={100}/>
-                        <Typography mt={1}>ISLAND TOUR</Typography>
-                    </Stack>
-                    <Stack direction={'column'} alignItems={'center'} sx={{color: 'gray'}}>
-                        <Image src={explorePic} alt={'Travel'} width={100} height={100}/>
-                        <Typography mt={1}>EXPLORE WORLD</Typography>
-                    </Stack>
-                </Stack>
-            </AppContainer>
-        </>
-    )
-}
+  return (
+    <>
+      <AppContainer>
+        <Grid container spacing={3} mt={15}>
+          <Grid item xs={12} sm={6} lg={5}>
+            <Stack
+              direction={"column"}
+              alignItems={"center"}
+              justifyContent={"space-between"}
+            >
+              <EuclidText
+                variant={"h4"}
+                sx={{ fontWeight: 600, mt: 12 }}
+                text={"Rent it !"}
+              />
+              <RentSlider />
+            </Stack>
+            <Box mt={5}>
+              <OurPartnership />
+            </Box>
+          </Grid>
+          <Grid item xs={12} sm={6} lg={7}>
+            <Box
+              sx={{
+                p: 2,
+                width: "100%",
+                height: "80%",
+                backgroundColor: colors.mainColor,
+                borderTopRightRadius: 50,
+                borderBottomLeftRadius: 50,
+                boxShadow:
+                  "rgb(0 0 0 / 25%) 0 14px 45px, rgb(0 0 0 / 22%) 0 10px 18px",
+              }}
+            >
+              <Box
+                sx={{
+                  width: "100%",
+                  height: "100%",
+                  borderTopRightRadius: 50,
+                }}
+              >
+                <Image
+                  src={
+                    "https://cdn.leonardo.ai/users/2dc5dbea-cfc5-4a04-a311-747595418e49/generations/fc058562-9c90-488e-9c44-6d003fc800ba/Leonardo_Creative_Home_rental_about_us_page_photo_0.jpg"
+                  }
+                  width={200}
+                  height={200}
+                  alt={"Rent"}
+                  style={{
+                    background: "transparent",
+                    borderTopRightRadius: 50,
+                    borderBottomLeftRadius: 50,
+                    width: "100%",
+                    height: "100%",
+                  }}
+                />
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} my={5}>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+            <Image
+              src={rentMiddleImage}
+              width={400}
+              height={400}
+              alt={"Rent"}
+            />
+          </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={6}>
+            <Typography variant={"caption"} sx={{ fontWeight: 600 }}>
+              Rent your place
+            </Typography>
+            <Typography variant={"h6"} sx={{ fontWeight: 700, marginY: 2 }}>
+              One-to-one guidance from a superhost
+            </Typography>
+            <Typography variant={"caption"}>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Architecto, assumenda et facilis in inventore ipsa maxime neque
+              quibusdam rem saepe, suscipit ullam veritatis! Cum fugit nam
+              nostrum sint! Distinctio, minus.
+            </Typography>
+            <Grid container my={2}>
+              <Grid item xs={6}>
+                <Typography sx={{ fontWeight: 600 }}>100+</Typography>
+                <Typography
+                  variant={"caption"}
+                  sx={{ fontSize: 10, lineHeight: 2 }}
+                >
+                  Lorem
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography sx={{ fontWeight: 600 }}>172</Typography>
+                <Typography
+                  variant={"caption"}
+                  sx={{ fontSize: 10, lineHeight: 2 }}
+                >
+                  Places
+                </Typography>
+              </Grid>
+              <Grid item xs={6} py={1}>
+                <Typography sx={{ fontWeight: 600 }}>68</Typography>
+                <Typography
+                  variant={"caption"}
+                  sx={{ fontSize: 10, lineHeight: 2 }}
+                >
+                  Lorem
+                </Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography sx={{ fontWeight: 600 }}>32M+</Typography>
+                <Typography
+                  variant={"caption"}
+                  sx={{ fontSize: 10, lineHeight: 2 }}
+                >
+                  We help to find your dream place
+                </Typography>
+              </Grid>
+            </Grid>
+            <ColorButton
+              size={"small"}
+              variant={"contained"}
+              sx={{ borderRadius: 25, m: 1, textTransform: "capitalize" }}
+              onClick={() => router.push("/rent-your-place/form")}
+            >
+              Rent your place
+            </ColorButton>
+          </Grid>
+        </Grid>
+        <Stack direction={"row"} justifyContent={"space-between"} pb={5}>
+          <Stack
+            direction={"column"}
+            alignItems={"center"}
+            sx={{ color: "#A8A7AB" }}
+          >
+            <Image src={travelPic} alt={"Travel"} width={100} height={100} />
+            <Typography mt={1}>TRAVEL</Typography>
+          </Stack>
+          <Stack
+            direction={"column"}
+            alignItems={"center"}
+            sx={{ color: "gray" }}
+          >
+            <Image src={cityPic} alt={"Travel"} width={100} height={100} />
+            <Typography mt={1}>CITY TOUR</Typography>
+          </Stack>
+          <Stack
+            direction={"column"}
+            alignItems={"center"}
+            sx={{ color: "gray" }}
+          >
+            <Image src={islandPic} alt={"Travel"} width={100} height={100} />
+            <Typography mt={1}>ISLAND TOUR</Typography>
+          </Stack>
+          <Stack
+            direction={"column"}
+            alignItems={"center"}
+            sx={{ color: "gray" }}
+          >
+            <Image src={explorePic} alt={"Travel"} width={100} height={100} />
+            <Typography mt={1}>EXPLORE WORLD</Typography>
+          </Stack>
+        </Stack>
+      </AppContainer>
+    </>
+  );
+};
 export default RentYourPlace;
 
-RentYourPlace.getLayout = (page: ReactElement) =>
-    <Layout>{page}</Layout>
+RentYourPlace.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
