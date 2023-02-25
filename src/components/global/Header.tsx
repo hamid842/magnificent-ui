93da20895972a797d4f58e90ca74fff1be003a97
Mehdi <mjv@medals.ai>
2023-02-25 11:45:02 +0330

    seo

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,195 +1,223 @@
-import {MouseEvent, useContext, useState} from "react";
+import { MouseEvent, useContext, useState } from "react";
 // next.js
 import Image from "next/image";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 // material-ui
 import {
-    AppBar,
-    Avatar,
-    Box,
-    Button,
-    ButtonProps,
-    IconButton,
-    Menu,
-    MenuItem,
-    Stack,
-    Toolbar,
-    Typography
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  ButtonProps,
+  IconButton,
+  Menu,
+  MenuItem,
+  Stack,
+  Toolbar,
+  Typography,
 } from "@mui/material";
-import {styled} from '@mui/material/styles';
-import MenuIcon from '@mui/icons-material/Menu';
-import {Person} from "@mui/icons-material";
+import { styled } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Person } from "@mui/icons-material";
 // project imports
 import logo from "../../../public/header/main-logo.png";
-import {euclidFont, switzerFont} from "@/assets/fonts";
-import colors from '@/assets/colors'
+import { euclidFont, switzerFont } from "@/assets/fonts";
+import colors from "@/assets/colors";
 import AuthWrapper from "@/auth/AuthWrapper";
-import {AuthContext} from "../../../context/contexts";
+import { AuthContext } from "../../../context/contexts";
 
 const navPages = [
-    {label: 'Home', path: "/"},
-    {label: 'Our Homes', path: "/our-homes"},
-    {label: 'Blogs', path: '/blogs'},
-    {label: 'About us', path: '/about'},
-    {label: 'Contact Us', path: '/contact-us'}
+  { label: "Home", path: "/" },
+  { label: "Our Homes", path: "/our-homes" },
+  { label: "Blogs", path: "/blogs" },
+  { label: "About us", path: "/about" },
+  { label: "Contact Us", path: "/contact-us" },
 ];
 
 export const ColorButton = styled(Button)<ButtonProps>({
-    color: "white",
+  color: "white",
+  backgroundColor: colors.mainColor,
+  "&:hover": {
     backgroundColor: colors.mainColor,
-    '&:hover': {
-        backgroundColor: colors.mainColor,
-    },
+  },
 });
 
 //========================|| Header ||=============================//
 
 export default function Header() {
-    const router = useRouter();
-    const {user} = useContext(AuthContext)
-    const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
-    const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+  const router = useRouter();
+  const { user } = useContext(AuthContext);
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
-    const menuItemsStyles = {
-        fontSize: 16,
-        fontWeight: 500,
-        ml: 1,
-        display: 'block',
-        textTransform: "capitalize",
-        color: colors.navMenuColor,
-        borderBottom: `2px solid transparent`,
-        '&:hover': {
-            color: colors.mainColor,
-            borderRadius: 0,
-            background: 'transparent',
-            borderBottom: `2px solid ${colors.mainColor}`,
+  const menuItemsStyles = {
+    fontSize: 16,
+    fontWeight: 500,
+    ml: 1,
+    display: "block",
+    textTransform: "capitalize",
+    color: colors.navMenuColor,
+    borderBottom: `2px solid transparent`,
+    "&:hover": {
+      color: colors.mainColor,
+      borderRadius: 0,
+      background: "transparent",
+      borderBottom: `2px solid ${colors.mainColor}`,
+    },
+  };
 
-        }
-    }
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
-        setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
-        setAnchorElUser(event.currentTarget);
-    };
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-    const handleCloseNavMenu = () => {
-        setAnchorElNav(null);
-    };
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
-    };
+  return (
+    <AppBar
+      position="static"
+      sx={{
+        width: "100vw",
+        px: 1,
+        py: 2,
+        position: "absolute",
+        top: 0,
+        zIndex: 1000,
+        background: colors.bodyBackground,
+        boxShadow: "none",
+      }}
+    >
+      <Toolbar disableGutters>
+        <Box sx={{ display: { xs: "none", sm: "none", md: "flex" } }}>
+          <Image
+            src={logo}
+            alt={"Logo"}
+            width={210}
+            height={98}
+            style={{ cursor: "pointer" }}
+            onClick={() => router.push("/")}
+          />
+        </Box>
+        <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleOpenNavMenu}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorElNav}
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "left",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "left",
+            }}
+            open={Boolean(anchorElNav)}
+            onClose={handleCloseNavMenu}
+          >
+            {navPages.map((page) => (
+              <MenuItem
+                key={page.label}
+                value={page.path}
+                onClick={() => router.push(page.path)}
+              >
+                <Typography textAlign="center" sx={{ fontSize: 18 }}>
+                  {page.label}
+                </Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+        <Box
+          sx={{
+            flexGrow: 1,
+            ml: 2,
+            display: { xs: "none", md: "flex" },
+          }}
+        >
+          {navPages.map((page) => (
+            <Button
+              key={page.label}
+              size={"small"}
+              value={page.path}
+              onClick={() => router.push(page.path)}
+              className={switzerFont.className}
+              sx={menuItemsStyles}
+            >
+              {page.label}
+            </Button>
+          ))}
+        </Box>
+        {/* ========================= || Setting (when click on header avatar) || ====================== */}
+        <Box sx={{ flexGrow: 0, mr: 2 }}>
+          <Stack direction={"row"}>
+            <ColorButton
+              size={"small"}
+              variant={"contained"}
+              className={euclidFont.className}
+              sx={{
+                textTransform: "none",
+                borderRadius: 25,
+                fontSize: 12,
+                mr: 1,
+              }}
+              onClick={() => router.push("/rent-your-place")}
+            >
+              Rent your place
+            </ColorButton>
 
-    return (
-        <AppBar position="static" sx={{
-            width: '100vw',
-            px: 1,
-            py: 2,
-            position: 'absolute',
-            top: 0,
-            zIndex: 1000,
-            background: colors.bodyBackground,
-            boxShadow: "none"
-        }}>
-            <Toolbar disableGutters>
-                <Box sx={{display: {xs: 'none', sm: 'none', md: 'flex'}}}>
-                    <Image
-                        src={logo}
-                        alt={'Logo'}
-                        width={210}
-                        height={98}
-                        style={{cursor: 'pointer'}}
-                        onClick={() => router.push('/')}
-                    />
-                </Box>
-                <Box sx={{flexGrow: 1, display: {xs: 'flex', md: 'none'}}}>
-                    <IconButton
-                        size="large"
-                        aria-label="account of current user"
-                        aria-controls="menu-appbar"
-                        aria-haspopup="true"
-                        onClick={handleOpenNavMenu}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Menu
-                        id="menu-appbar"
-                        anchorEl={anchorElNav}
-                        anchorOrigin={{
-                            vertical: 'bottom',
-                            horizontal: 'left',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'left',
-                        }}
-                        open={Boolean(anchorElNav)}
-                        onClose={handleCloseNavMenu}
-                    >
-                        {navPages.map((page) => (
-                            <MenuItem key={page.label} value={page.path} onClick={() => router.push(page.path)}>
-                                <Typography textAlign="center" sx={{fontSize: 18}}>{page.label}</Typography>
-                            </MenuItem>
-                        ))}
-                    </Menu>
-                </Box>
-                <Box sx={{
-                    flexGrow: 1,
-                    ml: 2,
-                    display: {xs: 'none', md: 'flex'}
-                }}>
-                    {navPages.map((page) => (
-                        <Button
-                            key={page.label}
-                            size={'small'}
-                            value={page.path}
-                            onClick={() => router.push(page.path)}
-                            className={switzerFont.className}
-                            sx={menuItemsStyles}
-                        >
-                            {page.label}
-                        </Button>
-                    ))}
-                </Box>
-                {/* ========================= || Setting (when click on header avatar) || ====================== */}
-                <Box sx={{flexGrow: 0, mr: 2}}>
-                    <Stack direction={'row'}>
-                        <ColorButton size={'small'} variant={'contained'} className={euclidFont.className}
-                                     sx={{textTransform: 'none', borderRadius: 25, fontSize: 12, mr: 1}}
-                                     onClick={() => router.push('/rent-your-place')}
-                        >Calculate your income</ColorButton>
-
-                        <Avatar onClick={handleOpenUserMenu} aria-haspopup={true} sx={{cursor: 'pointer'}}>
-                            <Person/>
-                        </Avatar>
-                    </Stack>
-                    <Menu
-                        sx={{mt: '45px'}}
-                        id="menu-appbar"
-                        anchorEl={anchorElUser}
-                        anchorOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        keepMounted
-                        transformOrigin={{
-                            vertical: 'top',
-                            horizontal: 'right',
-                        }}
-                        open={Boolean(anchorElUser)}
-                        onClose={handleCloseUserMenu}
-                    >
-                        {!user ? <MenuItem onClick={handleCloseUserMenu}>
-                            <AuthWrapper isHeader={true}/>
-                        </MenuItem> : <>
-                            <Button>Logout</Button>
-                        </>}
-                    </Menu>
-                </Box>
-            </Toolbar>
-        </AppBar>
-    );
+            <Avatar
+              onClick={handleOpenUserMenu}
+              aria-haspopup={true}
+              sx={{ cursor: "pointer" }}
+            >
+              <Person />
+            </Avatar>
+          </Stack>
+          <Menu
+            sx={{ mt: "45px" }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {!user ? (
+              <MenuItem onClick={handleCloseUserMenu}>
+                <AuthWrapper isHeader={true} />
+              </MenuItem>
+            ) : (
+              <>
+                <Button>Logout</Button>
+              </>
+            )}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
 }
--- src/components/home/RentPlaceSection.tsx
+++ src/components/home/RentPlaceSection.tsx
@@ -1,61 +1,76 @@
 // Next.js
 import Image from "next/image";
-import {useRouter} from "next/router";
+import { useRouter } from "next/router";
 // Material-ui
-import {Box, Grid, Stack} from "@mui/material";
+import { Box, Grid, Stack } from "@mui/material";
 // Project imports
 import TitleSeparator from "@/components/global/TitleSeparator";
 import AppButton from "@/components/global/AppButton";
 import SwitzerText from "@/components/css-texts/SwitzerText";
 import colors from "@/assets/colors";
-import dictionaries from '../../../dictionaries/en.json';
-import rentImage from '../../../public/home/rent.jpg'
+import dictionaries from "../../../dictionaries/en.json";
+import rentImage from "../../../public/home/rent.jpg";
 
 //======================|| Rent Your Place Section ||==========================
 
 const RentPlaceSection = () => {
-    const router = useRouter();
-    return (
-        <Box>
-            <TitleSeparator separatorTitle={'With us'} title={'Rent your place'}/>
-            <Grid container spacing={3}>
-                <Grid item xs={12} sm={6} lg={6}>
-                    <Box sx={{
-                        p: 2,
-                        width: '100%',
-                        height: '100%',
-                        backgroundColor: colors.mainColor,
-                        borderTopLeftRadius: 50,
-                        boxShadow: 'rgb(0 0 0 / 25%) 0 14px 45px, rgb(0 0 0 / 22%) 0 10px 18px',
-                    }}>
-                        <Box sx={{
-                            width: '100%',
-                            height: '100%',
-                            borderTopLeftRadius: 50,
-                        }}>
-                            <Image src={rentImage} alt={"Pic"} width={400} height={310}
-                                   style={{
-                                       background: "transparent",
-                                       borderTopLeftRadius: 50,
-                                       borderBottomRightRadius: 50,
-                                       width: '100%'
-                                   }}/>
-                        </Box>
-                    </Box>
-                </Grid>
-                <Grid item xs={12} sm={6} lg={6}>
-                    <SwitzerText sx={{textAlign: 'justify', mb: 3}}
-                                 text={`${dictionaries.homePage.rentExplanation}`}/>
-                    <Stack alignItems={'center'}>
-
-                        <AppButton label={'See more'}
-                                   onClick={() => router.push('/rent-your-place')}/>
-                    </Stack>
-                </Grid>
-            </Grid>
-        </Box>
-
-    )
-}
+  const router = useRouter();
+  return (
+    <Box>
+      <TitleSeparator
+        separatorTitle={"With us"}
+        title={"Calculate your income"}
+      />
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} lg={6}>
+          <Box
+            sx={{
+              p: 2,
+              width: "100%",
+              height: "100%",
+              backgroundColor: colors.mainColor,
+              borderTopLeftRadius: 50,
+              boxShadow:
+                "rgb(0 0 0 / 25%) 0 14px 45px, rgb(0 0 0 / 22%) 0 10px 18px",
+            }}
+          >
+            <Box
+              sx={{
+                width: "100%",
+                height: "100%",
+                borderTopLeftRadius: 50,
+              }}
+            >
+              <Image
+                src={rentImage}
+                alt={"Pic"}
+                width={400}
+                height={310}
+                style={{
+                  background: "transparent",
+                  borderTopLeftRadius: 50,
+                  borderBottomRightRadius: 50,
+                  width: "100%",
+                }}
+              />
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={6} lg={6}>
+          <SwitzerText
+            sx={{ textAlign: "justify", mb: 3 }}
+            text={`${dictionaries.homePage.rentExplanation}`}
+          />
+          <Stack alignItems={"center"}>
+            <AppButton
+              label={"See more"}
+              onClick={() => router.push("/rent-your-place")}
+            />
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+};
 
 export default RentPlaceSection;
