ab9ace14f0df37e1eb778ea7c0ab17942c2612ee
Mehdi <mjv@medals.ai>
2023-02-25 17:33:48 +0330

    seo

--- src/pages/contact-us.tsx
+++ src/pages/contact-us.tsx
@@ -1,8 +1,8 @@
-import {ReactElement} from "react";
+import { ReactElement } from "react";
 // Next.js
 import dynamic from "next/dynamic";
 // Material ui
-import {Grid, Typography} from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 // Project imports
 import manager from "../../public/contact-us/general-manager.jpg";
 import specialist from "../../public/contact-us/specialist.jpg";
@@ -10,115 +10,148 @@ import chief from "../../public/contact-us/chief.jpg";
 import officer from "../../public/contact-us/officer.jpg";
 import sales from "../../public/contact-us/sales.jpg";
 
-const Layout = dynamic(() => import("@/components/global/Layout"))
-const AppContainer = dynamic(() => import("@/components/global/AppContainer"))
-const FirstSection = dynamic(() => import("@/components/contact/FirstSection"))
-const SecondSection = dynamic(() => import("@/components/contact/SecondSection"))
-const ZoomableImage = dynamic(() => import("@/components/global/ZoomableImage"))
+const Layout = dynamic(() => import("@/components/global/Layout"));
+const AppContainer = dynamic(() => import("@/components/global/AppContainer"));
+const FirstSection = dynamic(() => import("@/components/contact/FirstSection"));
+const SecondSection = dynamic(
+  () => import("@/components/contact/SecondSection")
+);
+const ZoomableImage = dynamic(
+  () => import("@/components/global/ZoomableImage")
+);
 
 //=========================|| Contact us ||===========================
 
 const ContactUs = () => {
+  const imageStyles = {
+    width: "100%",
+    height: "auto",
+    borderRadius: 5,
+    boxShadow: "rgb(0 0 0 / 25%) 0 14px 40px, rgb(0 0 0 / 22%) 0 10px 18px",
+  };
 
-    const imageStyles = {
-        width: "100%",
-        height: "auto",
-        borderRadius: 5,
-        boxShadow:
-            "rgb(0 0 0 / 25%) 0 14px 40px, rgb(0 0 0 / 22%) 0 10px 18px",
-    }
-
-    const boxHeight = {xs: 400, sm: 350, md: 220, lg: 230}
-    return (
-        <AppContainer>
-            <FirstSection/>
-            <SecondSection/>
-            <Grid container spacing={2} mt={5}>
-                <Grid item xs={12} sm={12} lg={12}>
-                    <Typography
-                        variant={"h4"}
-                        textAlign={"center"}
-                        sx={{fontWeight: 700}}
-                    >
-                        Our team
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} sm={12} lg={12}>
-                    <Grid container spacing={1.5}>
-                        <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
-                            <ZoomableImage
-                                src={manager}
-                                boxHeight={boxHeight}
-                                alt={"Pic"}
-                                sizes="90vw"
-                                style={imageStyles}
-                            />
-                            <Typography sx={{fontWeight: 600}} mt={2}>Ehsan Pakroo</Typography>
-                            <Typography variant={"caption"} sx={{color: "gray"}}>
-                                General Manager
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
-                            <ZoomableImage
-                                src={sales}
-                                boxHeight={boxHeight}
-                                alt={"Pic"}
-                                sizes="90vw"
-                                style={imageStyles}
-                            />
-                            <Typography sx={{fontWeight: 600}} mt={2}>
-                                Caitlin Camilleri
-                            </Typography>
-                            <Typography variant={"caption"} sx={{color: "gray"}}>
-                                Sales & Marketing Manager
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
-                            <ZoomableImage
-                                src={chief}
-                                boxHeight={boxHeight}
-                                alt={"Pic"}
-                                sizes="90vw"
-                                style={imageStyles}
-                            />
-                            <Typography sx={{fontWeight: 600}} mt={2}>Mahmoud Habib</Typography>
-                            <Typography variant={"caption"} sx={{color: "gray"}}>
-                                Chief Accountant
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
-                            <ZoomableImage
-                                src={officer}
-                                boxHeight={boxHeight}
-                                alt={"Pic"}
-                                sizes="90vw"
-                                style={imageStyles}
-                            />
-                            <Typography sx={{fontWeight: 600}} mt={2}>Manish Mohan</Typography>
-                            <Typography variant={"caption"} sx={{color: "gray"}}>
-                                Operations & Excellence Manager
-                            </Typography>
-                        </Grid>
-                        <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
-                            <ZoomableImage
-                                src={specialist}
-                                boxHeight={boxHeight}
-                                alt={"Pic"}
-                                sizes="90vw"
-                                style={imageStyles}
-                            />
-                            <Typography sx={{fontWeight: 600}} mt={2}>
-                                JHarish Allakonda
-                            </Typography>
-                            <Typography variant={"caption"} sx={{color: "gray"}}>
-                                Guest Relation Associate
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Grid>
+  const boxHeight = { xs: 400, sm: 350, md: 220, lg: 230 };
+  return (
+    <>
+      <NextSeo
+        title="Know More About Magnificent | Best Property Management Companies in Downtown Dubai"
+        description="Magnificent is one of the most valuable &amp; trustable property management companies providing luxury vacation rentals management services in downtown dubai"
+        canonical="https://magnificent.ae/about-us/"
+        openGraph={{
+          locale: "en_US",
+          type: "article",
+          title:
+            "Know More About Magnificent | Best Property Management Companies in Downtown Dubai",
+          description:
+            "Magnificent is one of the most valuable &amp; trustable property management companies providing luxury vacation rentals management services in downtown dubai",
+          url: "https://magnificent.ae/about-us/",
+          site_name: "Magnificent Holiday",
+          article: {
+            modifiedTime: "2023-01-07T13:57:45+00:00",
+          },
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+          //   label1: 'Est. reading time',
+          //   data1: '8 minutes',
+        }}
+      />
+      <AppContainer>
+        <FirstSection />
+        <SecondSection />
+        <Grid container spacing={2} mt={5}>
+          <Grid item xs={12} sm={12} lg={12}>
+            <Typography
+              variant={"h4"}
+              textAlign={"center"}
+              sx={{ fontWeight: 700 }}
+            >
+              Our team
+            </Typography>
+          </Grid>
+          <Grid item xs={12} sm={12} lg={12}>
+            <Grid container spacing={1.5}>
+              <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
+                <ZoomableImage
+                  src={manager}
+                  boxHeight={boxHeight}
+                  alt={"Pic"}
+                  sizes="90vw"
+                  style={imageStyles}
+                />
+                <Typography sx={{ fontWeight: 600 }} mt={2}>
+                  Ehsan Pakroo
+                </Typography>
+                <Typography variant={"caption"} sx={{ color: "gray" }}>
+                  General Manager
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
+                <ZoomableImage
+                  src={sales}
+                  boxHeight={boxHeight}
+                  alt={"Pic"}
+                  sizes="90vw"
+                  style={imageStyles}
+                />
+                <Typography sx={{ fontWeight: 600 }} mt={2}>
+                  Caitlin Camilleri
+                </Typography>
+                <Typography variant={"caption"} sx={{ color: "gray" }}>
+                  Sales & Marketing Manager
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
+                <ZoomableImage
+                  src={chief}
+                  boxHeight={boxHeight}
+                  alt={"Pic"}
+                  sizes="90vw"
+                  style={imageStyles}
+                />
+                <Typography sx={{ fontWeight: 600 }} mt={2}>
+                  Mahmoud Habib
+                </Typography>
+                <Typography variant={"caption"} sx={{ color: "gray" }}>
+                  Chief Accountant
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
+                <ZoomableImage
+                  src={officer}
+                  boxHeight={boxHeight}
+                  alt={"Pic"}
+                  sizes="90vw"
+                  style={imageStyles}
+                />
+                <Typography sx={{ fontWeight: 600 }} mt={2}>
+                  Manish Mohan
+                </Typography>
+                <Typography variant={"caption"} sx={{ color: "gray" }}>
+                  Operations & Excellence Manager
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={4} md={2.4} lg={2.4}>
+                <ZoomableImage
+                  src={specialist}
+                  boxHeight={boxHeight}
+                  alt={"Pic"}
+                  sizes="90vw"
+                  style={imageStyles}
+                />
+                <Typography sx={{ fontWeight: 600 }} mt={2}>
+                  JHarish Allakonda
+                </Typography>
+                <Typography variant={"caption"} sx={{ color: "gray" }}>
+                  Guest Relation Associate
+                </Typography>
+              </Grid>
             </Grid>
-        </AppContainer>
-    );
+          </Grid>
+        </Grid>
+      </AppContainer>
+    </>
+  );
 };
 export default ContactUs;
 
--- src/pages/our-homes.tsx
+++ src/pages/our-homes.tsx
@@ -1,103 +1,159 @@
-import {ChangeEvent, ReactElement, useEffect, useState} from "react";
+import { ChangeEvent, ReactElement, useEffect, useState } from "react";
 // Next.js
-import {Router, useRouter} from "next/router";
+import { Router, useRouter } from "next/router";
 // Material ui
-import {Box, Grid, Pagination, Stack, Typography} from "@mui/material";
+import { Box, Grid, Pagination, Stack, Typography } from "@mui/material";
 // Project imports
 import FilterDialog from "@/components/our-homes/FilterDialog";
 import SortByDropdown from "@/components/our-homes/SortByDropdown";
 import SpecialOffersItem from "@/components/global/SpecialOffersItem";
 import EuclidText from "@/components/css-texts/EuclidText";
-import {IProperty} from "@/utils/property-type";
-import {instance} from "@/config/axiosConfig";
+import { IProperty } from "@/utils/property-type";
+import { instance } from "@/config/axiosConfig";
 import AppContainer from "@/components/global/AppContainer";
-import {GetServerSideProps} from "next";
+import { GetServerSideProps } from "next";
 import Layout from "@/components/global/Layout";
+import { NextSeo } from "next-seo";
 
-export const getServerSideProps: GetServerSideProps = async ({query}) => {
-    const page = query.page || 1;
-    const response = await instance(`/properties?populate=*&pagination[page]=${page}&pagination[pageSize]=9`)
-    const properties = response.data?.data;
-    return {
-        props: {
-            properties
-        }
-    }
-}
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+  const page = query.page || 1;
+  const response = await instance(
+    `/properties?populate=*&pagination[page]=${page}&pagination[pageSize]=9`
+  );
+  const properties = response.data?.data;
+  return {
+    props: {
+      properties,
+    },
+  };
+};
 
 type LastMinuteDealsProps = {
-    properties: IProperty[],
-}
+  properties: IProperty[];
+};
 
 //======================|| Last Minute Deals ||============================
 
-const OurHomes = ({properties}: LastMinuteDealsProps) => {
-    const [isLoading, setLoading] = useState(false);
-    const startLoading = () => setLoading(true);
-    const stopLoading = () => setLoading(false);
-    const router = useRouter();
+const OurHomes = ({ properties }: LastMinuteDealsProps) => {
+  const [isLoading, setLoading] = useState(false);
+  const startLoading = () => setLoading(true);
+  const stopLoading = () => setLoading(false);
+  const router = useRouter();
 
-    useEffect(() => {
-        Router.events.on('routeChangeStart', startLoading);
-        Router.events.on('routeChangeComplete', stopLoading);
+  useEffect(() => {
+    Router.events.on("routeChangeStart", startLoading);
+    Router.events.on("routeChangeComplete", stopLoading);
 
-        return () => {
-            Router.events.off('routeChangeStart', startLoading);
-            Router.events.off('routeChangeComplete', stopLoading);
-        }
-    }, [])
-
-
-    const paginationHandler = (event: ChangeEvent<unknown>, value: number) => {
-        const path = router.pathname
-        const query = router.query
-        query.page = value.toString()
-        router.push({
-            pathname: path,
-            query: query,
-        }).then(() => {
-        })
+    return () => {
+      Router.events.off("routeChangeStart", startLoading);
+      Router.events.off("routeChangeComplete", stopLoading);
     };
+  }, []);
 
+  const paginationHandler = (event: ChangeEvent<unknown>, value: number) => {
+    const path = router.pathname;
+    const query = router.query;
+    query.page = value.toString();
+    router
+      .push({
+        pathname: path,
+        query: query,
+      })
+      .then(() => {});
+  };
 
-    return (
-        <Box mt={16}>
-            <EuclidText variant={'h4'} align={'center'} sx={{fontWeight: 700, mb: 5}} text={'Our Homes'}/>
-            {/*<SearchDestinationLastMin position={'relative'}/>*/}
-            <AppContainer>
-                <Grid container justifyContent={'space-between'}>
-                    <Grid item xs={6} sm={6} lg={6}>
-                    </Grid>
-                    <Grid item xs={6} sm={6} lg={6}>
-                        <Stack direction={'row'} alignItems={'center'} justifyContent={'flex-end'}>
-                            <FilterDialog/>
-                            <Typography>Sort by</Typography>
-                            <SortByDropdown/>
-                        </Stack>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={3} pb={8}>
-                    {!properties?.length ? <Grid item xs={12} sm={12} lg={12} sx={{textAlign: 'center'}}>
-                        Loading... </Grid> : properties?.map((property: IProperty) => {
-                        return (
-                            <Grid key={property.id} item xs={12} sm={6} md={4} lg={4}
-                                  onClick={() => router.push({
-                                      pathname: `/last-minute-deals/${property.id}`,
-                                      query: {propertyId: JSON.stringify(property.id)}
-                                  }, `/last-minute-deals/${property.id}`)} sx={{cursor: 'pointer'}}>
-                                <SpecialOffersItem data={property}/>
-                            </Grid>
-                        )
-                    })}
-
-                </Grid>
-                <Stack alignItems={'center'} pb={3}>
-                    <Pagination shape="rounded" onChange={paginationHandler} count={2}/>
-                </Stack>
-            </AppContainer>
-        </Box>
-    )
-}
+  return (
+    <>
+      <NextSeo
+        title="Know More About Magnificent | Best Property Management Companies in Downtown Dubai"
+        description="Magnificent is one of the most valuable &amp; trustable property management companies providing luxury vacation rentals management services in downtown dubai"
+        canonical="https://magnificent.ae/about-us/"
+        openGraph={{
+          locale: "en_US",
+          type: "article",
+          title:
+            "Know More About Magnificent | Best Property Management Companies in Downtown Dubai",
+          description:
+            "Magnificent is one of the most valuable &amp; trustable property management companies providing luxury vacation rentals management services in downtown dubai",
+          url: "https://magnificent.ae/about-us/",
+          site_name: "Magnificent Holiday",
+          article: {
+            modifiedTime: "2023-01-07T13:57:45+00:00",
+          },
+        }}
+        twitter={{
+          cardType: "summary_large_image",
+          //   label1: 'Est. reading time',
+          //   data1: '8 minutes',
+        }}
+      />
+      <Box mt={16}>
+        <EuclidText
+          variant={"h4"}
+          align={"center"}
+          sx={{ fontWeight: 700, mb: 5 }}
+          text={"Our Homes"}
+        />
+        {/*<SearchDestinationLastMin position={'relative'}/>*/}
+        <AppContainer>
+          <Grid container justifyContent={"space-between"}>
+            <Grid item xs={6} sm={6} lg={6}></Grid>
+            <Grid item xs={6} sm={6} lg={6}>
+              <Stack
+                direction={"row"}
+                alignItems={"center"}
+                justifyContent={"flex-end"}
+              >
+                <FilterDialog />
+                <Typography>Sort by</Typography>
+                <SortByDropdown />
+              </Stack>
+            </Grid>
+          </Grid>
+          <Grid container spacing={3} pb={8}>
+            {!properties?.length ? (
+              <Grid item xs={12} sm={12} lg={12} sx={{ textAlign: "center" }}>
+                Loading...{" "}
+              </Grid>
+            ) : (
+              properties?.map((property: IProperty) => {
+                return (
+                  <Grid
+                    key={property.id}
+                    item
+                    xs={12}
+                    sm={6}
+                    md={4}
+                    lg={4}
+                    onClick={() =>
+                      router.push(
+                        {
+                          pathname: `/last-minute-deals/${property.id}`,
+                          query: { propertyId: JSON.stringify(property.id) },
+                        },
+                        `/last-minute-deals/${property.id}`
+                      )
+                    }
+                    sx={{ cursor: "pointer" }}
+                  >
+                    <SpecialOffersItem data={property} />
+                  </Grid>
+                );
+              })
+            )}
+          </Grid>
+          <Stack alignItems={"center"} pb={3}>
+            <Pagination
+              shape="rounded"
+              onChange={paginationHandler}
+              count={2}
+            />
+          </Stack>
+        </AppContainer>
+      </Box>
+    </>
+  );
+};
 export default OurHomes;
 
-OurHomes.getLayout = (page: ReactElement) => <Layout>{page}</Layout>
+OurHomes.getLayout = (page: ReactElement) => <Layout>{page}</Layout>;
